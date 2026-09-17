fae52d62f0cb2027fe7514f4efdb3ea1b7eb83c7
marcioespindolamge-star <marcioespindola.mge@gmail.com>
2026-09-17 06:52:55 -0300

    Remove script antigo de restauração do login

--- scripts/restaurar_login.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-# Restaura somente o index para a ultima versao funcional anterior ao ajuste
-# que alterou identificadores internos ao trocar Lavoura por Area.
-subprocess.run(['git','checkout','57e3dc6db221bb24851965b19c07cd05abab8296','--','index.html'],check=True)
-print('INDEX_FUNCIONAL_RESTAURADO')
